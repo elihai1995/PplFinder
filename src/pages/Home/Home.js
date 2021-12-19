0b185d232bcfa8678426bb22976fe2bf675d7b62
Elihai Ben Avraham <80115363+elihai1995@users.noreply.github.com>
2021-12-19 13:05:43 +0200

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Text from "components/Text";
 import UserList from "components/UserList";
 import { usePeopleFetch } from "hooks";
 import * as S from "./style";
 
 const Home = () => {
-  const { users, isLoading } = usePeopleFetch();
+  const [countries, setCountries] = useState([]);
+  const { users, isLoading, setCountriesToFetch, setPageToFetch, setUsers } =
+    usePeopleFetch();
+
+  useEffect(() => {
+    setPageToFetch(1);
+    setUsers([]);
+    setCountriesToFetch(countries);
+  }, [countries]);
+
+  const handleScroll = (e) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    if (scrollHeight - scrollTop <= clientHeight + 5 && !isLoading) {
+      setPageToFetch((prev) => prev + 1);
+    }
+  };
 
   return (
     <S.Home>
@@ -15,7 +30,13 @@ const Home = () => {
             PplFinder
           </Text>
         </S.Header>
-        <UserList users={users} isLoading={isLoading} />
+        <UserList
+          users={users}
+          isLoading={isLoading}
+          countries={countries}
+          setCountries={setCountries}
+          handleScroll={handleScroll}
+        />
       </S.Content>
     </S.Home>
   );
