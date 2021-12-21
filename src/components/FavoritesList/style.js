b944c85aea50ed595da040983905fa5558be5441
Elihai Ben Avraham <80115363+elihai1995@users.noreply.github.com>
2021-12-21 13:21:51 +0200

    Add files via upload

---
+++ src/components/FavoritesList/style.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const UserList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 35px;
+  width: 500px;
+  height: calc(100vh - 300px);
+  margin-block-start: 30px;
+  overflow-y: auto;
+`;
+
+export const User = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  max-height: 128px;
+  gap: 2px;
+  overflow: hidden;
+`;
+
+export const UserPicture = styled.img`
+  border-radius: 45%;
+`;
+
+export const IconButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  transition: opacity 0.2s ease-in-out;
+`;
