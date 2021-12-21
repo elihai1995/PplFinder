7e265c6bb97b0a7707cf10320625ae612393a939
Elihai Ben Avraham <80115363+elihai1995@users.noreply.github.com>
2021-12-21 20:34:05 +0200

    Update FavoritesList.js

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -1,38 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid } from "@material-ui/core/";
-import FavoriteCard from "components/FavoriteCard";
+import Text from "components/Text";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import * as S from "./style";
 
-const FavoritesList = () => {
-  const [favoritesUsers, setFavoritesUsers] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleFavorites = (userEmail) => {
-    const favorites = JSON.parse(localStorage.getItem("favorites"));
-    const filterFavorites = favorites.filter((user) => user.email !== userEmail);
-    setFavoritesUsers(filterFavorites);
-    localStorage.setItem("favorites", JSON.stringify(filterFavorites));
-  };
-
-  useEffect(() => {
-    setFavoritesUsers(JSON.parse(localStorage.getItem("favorites")));
-    setIsLoading(false);
-  }, []);
-
-  return (
-    <Container maxWidth={false}>
-      <Grid container justify="center" spacing={3}>
-        {favoritesUsers?.map((user) => (
-            <FavoriteCard
-              name={user.name.first}
-              lastName={user.name.last}
-              img={user.picture.large}
-              country={user.location.country}
-              handleFavorites={handleFavorites}
-            />
-        ))}
-      </Grid>
-    </Container>
-  );
+const FavoritesList = ()=> {
+    const [updateFavoriteList, setUpdateFavoriteList] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    const removeFromFavorite = (user)=>{
+      let updateList = updateFavoriteList.filter(function(item){
+        return (item !== user);
+      })
+      setUpdateFavoriteList(updateList);
+      localStorage.setItem("favorites", JSON.stringify(updateList));
+    };
+    
+    useEffect(() => { 
+      setUpdateFavoriteList(JSON.parse(localStorage.getItem("favorites")));
+      setIsLoading(false);
+    }, []);
+ 
+    return (
+      <S.UserList>
+        <S.List>
+          {updateFavoriteList?.map((user, index) => {
+            return (
+              <S.User
+                key={index}
+                style={{ backgroundColor: "#00874d" }}
+              >
+                <S.UserPicture src={user?.picture.large} alt="" />
+                <S.UserInfo>
+                  <Text size="22px" bold>
+                    {user?.name.title} {user?.name.first} {user?.name.last}
+                  </Text>
+                  <Text size="14px">{user?.email}</Text>
+                  <Text size="14px">
+                    {user?.location.street.number} {user?.location.street.name}
+                  </Text>
+                  <Text size="14px">
+                    {user?.location.city} {user?.location.country}
+                  </Text>
+                </S.UserInfo>
+                <S.IconButtonWrapper isVisible={true} onClick={() => removeFromFavorite(user)}>
+                  <IconButton>
+                    <FavoriteIcon color="error" />
+                  </IconButton>
+                </S.IconButtonWrapper>
+              </S.User>
+            );
+          })}
+        </S.List>
+      </S.UserList>
+    );
 };
 
 export default FavoritesList;
