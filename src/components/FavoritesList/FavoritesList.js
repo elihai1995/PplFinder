197eab037cf472ac4d855cb50a78fc4f7e349a39
Elihai Ben Avraham <80115363+elihai1995@users.noreply.github.com>
2021-12-21 13:17:45 +0200

    Update FavoritesList.js

--- src/components/FavoritesList/FavoritesList.js
+++ src/components/FavoritesList/FavoritesList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid  } from "@material-ui/core/";
+import { Container, Grid } from "@material-ui/core/";
 import FavoriteCard from "components/FavoriteCard";
 import Spinner from "components/Spinner";
 
